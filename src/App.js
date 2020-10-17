5eeb5ea798fa9b03faff36ae9d1b1bbd3a36ea51
Paulo Barbosa <paulomiguelbarbosa@gmail.com>
2020-10-17 16:29:04 +0100

    add dropdown base

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from "react";
-import Accordion from "./Components/Accordion";
 import Search from "./Components/Search";
+import Dropdown from "./Components/Dropdown";
 
-const items = [
-  {
-    title: "What is React?",
-    content: "React is a front end javascript framework"
-  },
-  {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers"
-  },
-  {
-    title: "How do you use React?",
-    content: "You use React by creating components"
-  }
+const options = [
+  { label: "red color", value: "red" },
+  { label: "green color", value: "green" },
+  { label: "blue color", value: "blue" }
 ];
 
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
