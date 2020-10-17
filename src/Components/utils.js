723aa1d3d833ce8b62a8a09198f2fb2bb992280e
Paulo Barbosa <paulomiguelbarbosa@gmail.com>
2020-10-17 15:09:03 +0100

    initial commit

--- src/Components/utils.js
+++ src/Components/utils.js
@@ -6,11 +6,15 @@ const axios = {
           data: [
             {
               pageid: "1",
-              snippet: "snippet 1",
-              title: Math.random() > 0.5 ? "title 1" : "title 1.1"
+              snippet: "Alto mafioso",
+              title: Math.random() > 0.5 ? "Pinto Da Costa" : "Don Corleone"
             },
-            { pageid: "2", snippet: "snippet 2", title: "title 2" },
-            { pageid: "3", snippet: "snippet 3", title: "title 3" }
+            { pageid: "2", snippet: "Primeiro Ministro", title: "Tone Costa" },
+            {
+              pageid: "3",
+              snippet: "Presidente Republica",
+              title: "Marcelo Sousa"
+            }
           ]
         });
       }, Math.random() * 1000);
